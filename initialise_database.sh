e4cc4f9e14a1531041c958ee6df712b7ece7c24f
Kieran <k1phelan234@gmail.com>
2026-01-08 13:21:01 +0000

    Remove bash

--- initialise_database.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source .env
-export PGPASSWORD="$DATABASE_PASSWORD"
-
-psql -h "$DATABASE_IP" -U "$DATABASE_USERNAME" -tc "SELECT 1 FROM pg_database WHERE datname = 'museum'" | grep -q 1 || \
-psql -h "$DATABASE_IP" -U "$DATABASE_USERNAME" -c "CREATE DATABASE museum"
-
-psql -h "$DATABASE_IP" -U "$DATABASE_USERNAME" -d museum -f schema.sql
